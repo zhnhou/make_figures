8fe4d871b0ee5b0b8661ff287e2e443757ad010a
zhnhou <zhn.hou@gmail.com>
2016-03-19 18:21:15 -0500

    add restore_save and plot_spthfi_bandpower

--- sptxhfi/figure_sptxhfi.py
+++ sptxhfi/figure_sptxhfi.py
@@ -1,3 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.idl import readsav
+from sys import exit
+
+def restore_save(savfile):
+
+    n = readsav(savfile)
+    key = n.keys()
+    if (len(key) != 1):
+        exit(".sav file is not a combined end2end file")
+
+    num_bands = int(n[key[0]]['num_bands'][0])
+    bands = n[key[0]]['bands'][0]
+    dbs_data = n[key[0]]['dbs_data_combined'][0]
+    dbs_sims = n[key[0]]['dbs_sims_combined'][0] # (nsims, nspecs, nbands)
+
+    winminell = int(n[key[0]]['winminell'][0])
+    winmaxell = int(n[key[0]]['winmaxell'][0])
+
+    winfunc_data = n[key[0]]['winfunc_data_combined'][0]
+    winfunc_sims = n[key[0]]['winfunc_sims_combined'][0]
+
+    cov_sv    = n[key[0]]['cov_sv_combined'][0]
+    cov_noise = n[key[0]]['cov_noise_combined'][0]
+
+    d = {'num_bands':num_bands, 'bands':bands, 
+         'dbs_data':dbs_data, 'dbs_sims':dbs_sims,
+         'winminell':winminell, 'winmaxell':winmaxell,
+         'winfunc_data':winfunc_data, 'winfunc_sims':winfunc_sims,
+         'cov_sv':cov_sv, 'cov_noise':cov_noise}
+
+    return d
+
+
+def plot_spthfi_bandpower(pdf_file=None):
+    
+    spt150xspt150_file = ''
